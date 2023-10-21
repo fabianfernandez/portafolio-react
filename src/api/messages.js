5ce35c52ec85ffb33f016590c411e478959d36e4
fabianfernandez <fabian.fernandeza@sansano.usm.cl>
2023-10-21 15:12:00 -0300

    crud functions

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,5 +1,12 @@
-import { getRequest, postRequest } from "./api";
+import { deleteRequest, getRequest, postRequest, putRequest } from "./api";
 
 export const getMessages = () => getRequest("http://127.0.0.1:8000/messages/");
 
-export const createMessage = (data) => postRequest("message/create/", data);
+export const createMessage = (data) =>
+  postRequest("http://127.0.0.1:8000/messages/create/", data);
+
+export const deleteMessage = (id) =>
+  deleteRequest(`http://127.0.0.1:8000/messages/delete/${id}/`);
+
+export const editMessage = (id, data) =>
+  putRequest(`http://127.0.0.1:8000/messages/update/${id}/`, data);
--- src/web/Web.jsx
+++ src/web/Web.jsx
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Row, Table, Typography } from "antd";
-import { getMessages } from "../api/messages";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Input,
+  Modal,
+  Row,
+  Space,
+  Table,
+  Typography,
+  message,
+} from "antd";
+import {
+  createMessage,
+  deleteMessage,
+  editMessage,
+  getMessages,
+} from "../api/messages";
+import { EyeOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const { Title, Paragraph } = Typography;
 
 export default function Web() {
   const [data, setData] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [formMode, setFormMode] = useState(null);
+  const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
   const columns = [
     {
       title: "Creador",
@@ -18,6 +41,13 @@ export default function Web() {
     {
       title: "Acciones",
       dataIndex: "actions",
+      render: (_, record) => (
+        <Space size="middle">
+          <EyeOutlined onClick={() => onShowMessage(record)} />
+          <EditOutlined onClick={() => onEditMessage(record)} />
+          <DeleteOutlined onClick={() => onDeleteMessage(record)} />
+        </Space>
+      ),
     },
   ];
   useEffect(() => {
@@ -26,8 +56,71 @@ export default function Web() {
         setData(data);
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [loading]);
 
+  const onSaveMessage = () => {
+    const data = form.getFieldsValue();
+    createMessage(data)
+      .then(() => {
+        messageApi.success("Mensaje creado");
+        setLoading(!loading);
+      })
+      .catch(() => messageApi.error("Ha ocurrido un error con el servidor"));
+    setIsOpen(false);
+  };
+
+  const onSaveEditMessage = () => {
+    const data = form.getFieldsValue();
+    editMessage(data.id, data)
+      .then(() => {
+        messageApi.success("Mensaje editado");
+        setLoading(!loading);
+      })
+      .catch(() => messageApi.error("Ha ocurrido un error con el servidor"));
+    setIsOpen(false);
+  };
+
+  const onDeleteMessage = (record) => {
+    deleteMessage(record.id)
+      .then(() => {
+        messageApi.success("Mensaje eliminado");
+        setLoading(!loading);
+      })
+      .catch(() => messageApi.error("Ha ocurrido un error con el servidor"));
+  };
+
+  const onShowMessage = (record) => {
+    setIsOpen(true);
+    form.setFieldsValue(record);
+    setFormMode(null);
+  };
+
+  const onEditMessage = (record) => {
+    setIsOpen(true);
+    form.setFieldsValue(record);
+    setFormMode("edit");
+  };
+
+  const onCreateMessage = () => {
+    setIsOpen(true);
+    form.resetFields();
+    setFormMode("create");
+  };
+
+  const footer = () => (
+    <>
+      {formMode === "create" && (
+        <Button type="primary" onClick={onSaveMessage}>
+          Guardar
+        </Button>
+      )}
+      {formMode === "edit" && (
+        <Button type="primary" onClick={onSaveEditMessage}>
+          Editar
+        </Button>
+      )}
+    </>
+  );
   return (
     <div style={{ margin: 20 }}>
       <Card>
@@ -42,14 +135,48 @@ export default function Web() {
             </Paragraph>
           </Col>
           <Col span={12}>
-            <Table
-              columns={columns}
-              dataSource={data}
-              rowKey={(record) => record.id}
-            />
+            {contextHolder}
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Table
+                columns={columns}
+                dataSource={data}
+                rowKey={(record) => record.id}
+              />
+              <Button
+                type="primary"
+                style={{ alignSelf: "center" }}
+                onClick={onCreateMessage}
+              >
+                Crear Mensaje
+              </Button>
+            </div>
           </Col>
         </Row>
       </Card>
+      <Modal
+        open={isOpen}
+        onCancel={() => setIsOpen(false)}
+        title="Deja un mensaje"
+        footer={footer}
+      >
+        <Form layout="vertical" form={form} disabled={!formMode}>
+          <Form.Item label="Creador" name="sender">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Asunto" name="subject">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Mensaje" name="body">
+            <Input.TextArea rows={6} />
+          </Form.Item>
+          <Form.Item name="id" hidden />
+        </Form>
+      </Modal>
     </div>
   );
 }
